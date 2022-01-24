f7b8b2d96e7c0036521d2eddc35f0e3d33cb76f5
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-01-24 15:43:17 -0800

    added efects changed styles and file names

---
+++ src/components/Details.js
@@ -0,0 +1,73 @@
+const boxMe = {
+  marginRight: "16px",
+  backgroundColor: "grey",
+  padding: "8px 10px",
+  borderRadius: "8px",
+  margin: "4px 16px 4px 0px"
+};
+
+const section = {
+  margin: "16px 0px"
+};
+
+export const Details = ({ data, setToggleDetails }) => {
+  const {
+    Title,
+    Year,
+    // Rated,
+    // Released,
+    // Runtime,
+    // Genre,
+    // Director,
+    // Writer,
+    Actors,
+    Plot,
+    // Language,
+    BoxOffice,
+    Poster,
+    imdbRating,
+    Rated,
+    Runtime,
+    Genre
+  } = data;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        marginLeft: "8px",
+        maxWidth: "800px",
+        height: "fit-content",
+        backgroundColor: "#121212"
+      }}
+    >
+      <img src={Poster} alt="No Poster" style={{ height: "300px" }} />
+      <div id="decription" style={{ marginLeft: "16px" }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <h1 style={{ width: "80%" }}>{Title}</h1>
+          <h1 style={{ color: "lightblue" }}>{imdbRating}</h1>
+        </div>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          <p style={boxMe}>{Rated}</p>
+          <p style={boxMe}>{Runtime}</p>
+          <p style={boxMe}>{Genre}</p>
+        </div>
+        <div style={section}>
+          <h3>Plot</h3>
+          <p>{Plot}</p>
+        </div>
+        <div style={section}>
+          <h3>Actors</h3>
+          <p>{Actors}</p>
+          <button
+            onClick={() => {
+              setToggleDetails(false);
+            }}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
